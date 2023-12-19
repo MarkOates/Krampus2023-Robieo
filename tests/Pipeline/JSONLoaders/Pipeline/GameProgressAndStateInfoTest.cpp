60920fe2b664d567a0b1e64cda3b7febbcbe67eb
Mark Oates <markoates0@gmail.com>
2023-12-19 12:14:42 -0500

    * Add comparison test for #from_json#

--- tests/Pipeline/JSONLoaders/Pipeline/GameProgressAndStateInfoTest.cpp
+++ tests/Pipeline/JSONLoaders/Pipeline/GameProgressAndStateInfoTest.cpp
@@ -2,7 +2,7 @@
 #include <gtest/gtest.h>
 
 #include <Pipeline/JSONLoaders/Pipeline/GameProgressAndStateInfo.hpp>
-//#include <Pipeline/Testing/Comparison/Pipeline/GameProgressAndStateInfo.hpp>
+#include <Pipeline/Testing/Comparison/Pipeline/GameProgressAndStateInfo.hpp>
 
 
 TEST(Pipeline_JSONLoaders_Pipeline_GameProgressAndStateInfoTest,
@@ -29,20 +29,24 @@ R"({
 TEST(Pipeline_JSONLoaders_Pipeline_GameProgressAndStateInfoTest,
    from_json__loads_json_data_into_the_object)
 {
-   //Pipeline::GameProgressAndStateInfo game_progress_and_state_info;
+   Pipeline::GameProgressAndStateInfo game_progress_and_state_info;
 
-   //std::string json =
-//R"({
-  //"position": 1.5
-//})";
+   std::string json =
+R"({
+  "delivered_package_identifiers": [
+    "package1",
+    "package2"
+  ]
+})";
 
-   //nlohmann::json parsed_json = nlohmann::json::parse(json);
-   //parsed_json.get_to(game_progress_and_state_info);
+   nlohmann::json parsed_json = nlohmann::json::parse(json);
+   parsed_json.get_to(game_progress_and_state_info);
 
-   //Pipeline::JSONLoaders::Pipeline::GameProgressAndStateInfo expected(1.5);
+   Pipeline::GameProgressAndStateInfo expected;
+   expected.set_delivered_package_identifiers({"package1", "package2"});
 
-   //// TODO: add comparison
-   //EXPECT_EQ(expected, game_progress_and_state_info);
+   // TODO: add comparison
+   EXPECT_EQ(expected, game_progress_and_state_info);
 }
 
 
