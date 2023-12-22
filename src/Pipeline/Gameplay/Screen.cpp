674f6d72be386675aea43b7c3850e81835b3fb04
Mark Oates <markoates0@gmail.com>
2023-12-22 16:28:52 -0500

    * Add comment on saving progress

--- src/Pipeline/Gameplay/Screen.cpp
+++ src/Pipeline/Gameplay/Screen.cpp
@@ -1642,6 +1642,8 @@ void Screen::emit_event_to_save_progress()
       std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
       throw std::runtime_error("Screen::emit_event_to_save_progress: error: guard \"event_emitter\" not met");
    }
+   // TODO: Test this
+   // TODO: Consider using a constant rather than "save_progress"
    event_emitter->emit_game_event(AllegroFlare::GameEvent("save_progress"));
    return;
 }
