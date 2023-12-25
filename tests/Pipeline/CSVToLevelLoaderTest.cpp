3f0d90d010bb235610cec36a7d3dca5d3927802b
Mark Oates <markoates0@gmail.com>
2023-12-25 13:02:08 -0500

    * Add basic test for identifier loading

--- tests/Pipeline/CSVToLevelLoaderTest.cpp
+++ tests/Pipeline/CSVToLevelLoaderTest.cpp
@@ -6,16 +6,26 @@
 
 TEST(Pipeline_CSVToLevelLoaderTest, can_be_created_without_blowing_up)
 {
-   Pipeline::CSVToLevelLoader csvto_level_loader;
+   Pipeline::CSVToLevelLoader loader;
 }
 
 
 TEST(Pipeline_CSVToLevelLoaderTest, load__will_not_blow_up)
 {
-   Pipeline::CSVToLevelLoader csvto_level_loader;
-   csvto_level_loader.set_csv_full_path("/Users/markoates/Repos/Pipeline/bin/data/levels/universe.csv");
-   csvto_level_loader.load();
+   Pipeline::CSVToLevelLoader loader;
+   loader.set_csv_full_path("/Users/markoates/Repos/Pipeline/bin/data/levels/universe.csv");
+   loader.load();
    SUCCEED();
+}
+
+
+TEST(Pipeline_CSVToLevelLoaderTest, load__when_loading_from_the_source_data__will_return_the_expected_values)
+{
+   Pipeline::CSVToLevelLoader loader;
+   loader.set_csv_full_path("/Users/markoates/Repos/Pipeline/bin/data/levels/universe.csv");
+   loader.load();
+
+   EXPECT_EQ(true, loader.level_exists("level-4"));
 }
 
 
