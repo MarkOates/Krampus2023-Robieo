f2ecd1c28706dbcc954af7de39b185b1a6d449c5
Mark Oates <markoates0@gmail.com>
2023-12-25 12:00:04 -0500

    * Add level obtaining from CSVToLevelLoader

--- include/Pipeline/CSVToLevelLoader.hpp
+++ include/Pipeline/CSVToLevelLoader.hpp
@@ -2,6 +2,8 @@
 
 
 #include <AllegroFlare/CSVParser.hpp>
+#include <Pipeline/Gameplay/Level.hpp>
+#include <map>
 #include <string>
 
 
@@ -11,6 +13,8 @@ namespace Pipeline
    {
    private:
       std::string csv_full_path;
+      std::map<std::string, Pipeline::Gameplay::Level> levels;
+      bool loaded;
 
    protected:
 
@@ -21,6 +25,9 @@ namespace Pipeline
 
       void set_csv_full_path(std::string csv_full_path);
       std::string get_csv_full_path() const;
+      std::map<std::string, Pipeline::Gameplay::Level> get_levels();
+      bool level_exists(std::string level_identifier="[unset-level_identifier]");
+      Pipeline::Gameplay::Level get_copy_of_level(std::string level_identifier="[unset-level_identifier]");
       void load();
    };
 }
--- src/Pipeline/CSVToLevelLoader.cpp
+++ src/Pipeline/CSVToLevelLoader.cpp
@@ -4,6 +4,9 @@
 
 #include <AllegroFlare/CSVParser.hpp>
 #include <AllegroFlare/UsefulPHP.hpp>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
 
 
 namespace Pipeline
@@ -13,6 +16,8 @@ namespace Pipeline
 CSVToLevelLoader::CSVToLevelLoader()
    : AllegroFlare::CSVParser()
    , csv_full_path("/Users/markoates/Repos/SurviveTheCity/bin/data/levels/universe.csv")
+   , levels({})
+   , loaded(false)
 {
 }
 
@@ -34,8 +39,58 @@ std::string CSVToLevelLoader::get_csv_full_path() const
 }
 
 
+std::map<std::string, Pipeline::Gameplay::Level> CSVToLevelLoader::get_levels()
+{
+   if (!(loaded))
+   {
+      std::stringstream error_message;
+      error_message << "[CSVToLevelLoader::get_levels]: error: guard \"loaded\" not met.";
+      std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
+      throw std::runtime_error("CSVToLevelLoader::get_levels: error: guard \"loaded\" not met");
+   }
+   return levels;
+}
+
+bool CSVToLevelLoader::level_exists(std::string level_identifier)
+{
+   if (!(loaded))
+   {
+      std::stringstream error_message;
+      error_message << "[CSVToLevelLoader::level_exists]: error: guard \"loaded\" not met.";
+      std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
+      throw std::runtime_error("CSVToLevelLoader::level_exists: error: guard \"loaded\" not met");
+   }
+   return (levels.find(level_identifier) != levels.end());
+}
+
+Pipeline::Gameplay::Level CSVToLevelLoader::get_copy_of_level(std::string level_identifier)
+{
+   if (!(loaded))
+   {
+      std::stringstream error_message;
+      error_message << "[CSVToLevelLoader::get_copy_of_level]: error: guard \"loaded\" not met.";
+      std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
+      throw std::runtime_error("CSVToLevelLoader::get_copy_of_level: error: guard \"loaded\" not met");
+   }
+   if (!(level_exists(level_identifier)))
+   {
+      std::stringstream error_message;
+      error_message << "[CSVToLevelLoader::get_copy_of_level]: error: guard \"level_exists(level_identifier)\" not met.";
+      std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
+      throw std::runtime_error("CSVToLevelLoader::get_copy_of_level: error: guard \"level_exists(level_identifier)\" not met");
+   }
+   return levels[level_identifier];
+}
+
 void CSVToLevelLoader::load()
 {
+   if (!((!loaded)))
+   {
+      std::stringstream error_message;
+      error_message << "[CSVToLevelLoader::load]: error: guard \"(!loaded)\" not met.";
+      std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
+      throw std::runtime_error("CSVToLevelLoader::load: error: guard \"(!loaded)\" not met");
+   }
    //std::string HARD_CODED_DB_FILE = "/Users/markoates/Repos/SurviveTheCity/bin/data/levels/universe.csv";
    //std::filesystem::exists(HARD_CODED_DB_FILE);
    std::string content = AllegroFlare::php::file_get_contents(csv_full_path);
@@ -44,6 +99,7 @@ void CSVToLevelLoader::load()
    csv_parser.set_raw_csv_content(content);
    csv_parser.parse();
    csv_parser.assemble_column_headers(2);
+   loaded = true;
    return;
 }
 
